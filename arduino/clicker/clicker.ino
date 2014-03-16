02a622336bea088d0c486f7d37959c7e1686fdcb
Eddie <eddie@gamezgalaxy.com>
2014-03-16 12:16:31 -0400

    Added motor control to protocol

--- arduino/clicker/clicker.ino
+++ arduino/clicker/clicker.ino
@@ -4,14 +4,30 @@
 int leftClickPort = 9                                                                                                                                                                         ;
 int rightClickPort = 10;
 
+int motorXEnable = 6;
+int motorX1 = 5;
+int motorX2 = 2;
+
+int motorYEnable = 3;
+int motorY1 = 4;
+int motorY2 = 1;
+
 //Clicking vars
-int leftClickDown = 35;
+int leftClickDown = 25;
 int leftClickUp = 15;
 int rightClickDown = 35;
 int rightClickUp = 25;
 Servo leftClick; Servo rightClick;
 
 void setup() {
+  pinMode(motorXEnable, OUTPUT);
+  pinMode(motorX1, OUTPUT);
+  pinMode(motorX2, OUTPUT);
+  
+  pinMode(motorYEnable, OUTPUT);
+  pinMode(motorY1, OUTPUT);
+  pinMode(motorY2, OUTPUT);
+  
   leftClick.attach(leftClickPort);
   rightClick.attach(rightClickPort);
   leftClick.write(leftClickUp);
@@ -34,6 +50,42 @@ void loop() {
     case 8: //Right up
       rightClick.write(rightClickUp);
       break;
+    case 16: //Move X Motor
+      byte power = Serial.read();
+      byte dir = Serial.read();
+      moveX(power, dir);
+      break;
+    case 32: //Move Y Motor
+      byte power = Serial.read();
+      byte dir = Serial.read();
+      moveY(power, dir);
+      break;
+    case 64: //Stop
+      stopMotors();
+      break;
   } 
  }
+}
+
+void moveX(byte p, byte dir) {
+ int power = int(p);
+ boolean reverse = false;
+ if (dir == 1) reverse = true;
+ analogWrite(motorXEnable, power);
+ digitalWrite(motorX1, !reverse);
+ digitalWrite(motorX2, reverse);
+}
+
+void moveY(byte p, byte dir) {
+ int power = int(p);
+ boolean reverse = false;
+ if (dir == 1) reverse = true;
+ analogWrite(motorYEnable, power);
+ digitalWrite(motorY1, !reverse);
+ digitalWrite(motorY2, reverse);
+}
+
+void stopMotors() {
+  moveX(0, 0);
+  moveY(0, 0);
 }
